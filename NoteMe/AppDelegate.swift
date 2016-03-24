708e30185403b7bad9db1b8033e388626d70cfad
Pierre Toroyan <p.toroyan@gmail.com>
2016-03-23 23:54:02 -0600

    App Delegate (Fabric)
    - Fabric setup in App Delegate

--- NoteMe/AppDelegate.swift
+++ NoteMe/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        //Fabric.with([Digits.self])
+        Fabric.with([Digits.self])
 
         return true
     }
